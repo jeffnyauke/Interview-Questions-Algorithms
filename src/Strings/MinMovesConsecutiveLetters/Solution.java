51452d6fdb85c542da85613a3ed3f666016c4f87
Jeffrey Nyauke <jeffnyauke@gmail.com>
2020-10-04 17:58:38 +0300

    Min Moves to Obtain String Without 3 Identical Consecutive Letters

---
+++ src/Strings/MinMovesConsecutiveLetters/Solution.java
@@ -0,0 +1,22 @@
+package Strings.MinMovesConsecutiveLetters;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(minMoves("baaaaa"));
+        System.out.println(minMoves("baaabbaabbbaa"));
+        System.out.println(minMoves("baabab"));
+    }
+
+    public static int minMoves(String s) {
+        int moves = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int runLength = 1;
+            for (; i + 1 < s.length() && s.charAt(i) == s.charAt(i + 1); i++){
+                runLength++;
+            }
+            moves += runLength / 3;
+        }
+        return moves;
+    }
+}
